3e4516f14652215ff80dfeb43f5cf82fe3a9a126
Michael Garcia <48392195+mickeySTI@users.noreply.github.com>
2023-04-22 01:59:59 -0400

    user is now able to update votes but it is currently working when all facts are displayed under all category

--- api.js
+++ api.js
@@ -21,6 +21,7 @@ export async function getFacts() {
         let ul = document.getElementById("list");
         facts.forEach((i) => {
           let category = CATEGORIES.find((x) => x.name === i.category);
+          let id = i.id;
 
           ul.insertAdjacentHTML(
             "afterbegin",
@@ -31,12 +32,45 @@ export async function getFacts() {
       target="_blank">(Source)</a></p>
       <span class="tag" style="background-color:${category.color}" >${i.category}</span>
       <div class="btn-group">
-          <button>👍 ${i.voteInteresting}</button>
-          <button>😧 ${i.voteMindBlowing}</button>
-          <button>⛔ ${i.voteFalse}</button>
+          <button class="voteInteresting">👍 ${i.voteInteresting}</button>
+          <button class="voteMindBlowing">🤯 ${i.voteMindBlowing}</button>
+          <button class="voteFalse">⛔ ${i.voteFalse}</button>
       </div>
     </li>`
           );
+
+          let voteBtn1 = document.querySelector(".voteInteresting");
+          let voteBtn2 = document.querySelector(".voteMindBlowing");
+          let voteBtn3 = document.querySelector(".voteFalse");
+
+          let obj = {};
+
+          voteBtn1.addEventListener("click", () => {
+            let count;
+            count = Number(voteBtn1.innerHTML.replace("👍", "")) + 1;
+            console.log(count);
+            obj[`voteInteresting`] = count;
+            updateVote(id, obj);
+            voteBtn1.innerHTML = `👍 ${count}`;
+          });
+
+          voteBtn2.addEventListener("click", () => {
+            let count;
+            count = Number(voteBtn2.innerHTML.replace("🤯", "")) + 1;
+            obj[`voteMindBlowing`] = count;
+            updateVote(id, obj);
+            voteBtn2.innerHTML = `🤯 ${count}`;
+            console.log(count);
+          });
+
+          voteBtn3.addEventListener("click", () => {
+            let count;
+            count = Number(voteBtn3.innerHTML.replace("⛔", "")) + 1;
+            obj[`voteFalse`] = count;
+            updateVote(id, obj);
+            voteBtn3.innerHTML = `⛔ ${count}`;
+            console.log(count);
+          });
         });
       });
   } catch (error) {
@@ -66,6 +100,27 @@ export async function createFact(obj) {
       }),
     }).then(() => {
       getFacts();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateVote(id, obj) {
+  let url = `https://qhsxmcuoqnyhlmfwwkct.supabase.co/rest/v1/facts?id=eq.${id}`;
+
+  try {
+    await fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Prefer: "return=minimal",
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoc3htY3VvcW55aGxtZnd3a2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEwMzk0ODYsImV4cCI6MTk5NjYxNTQ4Nn0.pAC7MMzH0CtvgT63TU1iEE8hEdBvVoSt95RWKFuQyEY",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoc3htY3VvcW55aGxtZnd3a2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEwMzk0ODYsImV4cCI6MTk5NjYxNTQ4Nn0.pAC7MMzH0CtvgT63TU1iEE8hEdBvVoSt95RWKFuQyEY",
+      },
+      body: JSON.stringify({ ...obj }),
     });
   } catch (error) {
     console.log(error);
